a14bb4cdfa4e16a6a7817ed00bebbbbf8f6396c4
Lunatine <github@lunatine.dev>
2026-05-10 16:11:41 +0100

    refactor: Adjust swagger docs for http types

--- services/api/src/types/http.ts
+++ services/api/src/types/http.ts
@@ -1,10 +1,23 @@
 import { Type, Static } from "typebox";
 
-export const ErrorResponse = Type.Object({
-    statusCode: Type.Number(),
-    error: Type.String(),
-    message: Type.String(),
-});
+export const ErrorResponse = Type.Object(
+    {
+        statusCode: Type.Number(),
+        error: Type.String(),
+        message: Type.String(),
+    },
+    {
+        $id: "ErrorResponse",
+        description: "Standard Error Response",
+        examples: [
+            {
+                statusCode: 500,
+                error: "Internal Server Error",
+                message: "There was an error processing your request",
+            },
+        ],
+    },
+);
 export type ErrorResponseType = Static<typeof ErrorResponse>;
 
 export const RedirectResponse = Type.Null();
