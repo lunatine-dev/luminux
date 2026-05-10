eadb24d390021dce5b364e4f55787f2c281f294e
Lunatine <github@lunatine.dev>
2026-05-10 16:05:07 +0100

    refactor: Adjust rate limit to match expected output from the schema

--- services/api/src/plugins/external/rate-limit.ts
+++ services/api/src/plugins/external/rate-limit.ts
@@ -1,10 +1,22 @@
 import fastifyRateLimit from "@fastify/rate-limit";
-import { FastifyInstance } from "fastify";
+import { FastifyInstance, FastifyRequest } from "fastify";
 
 export const autoConfig = (fastify: FastifyInstance) => {
     return {
-        max: fastify.config.RATE_LIMIT_MAX,
+        redis: fastify.redis,
+        global: false,
+        max: fastify.config.RATE_LIMIT_MAX || 100,
         timeWindow: "1 minute",
+
+        errorResponseBuilder: (request: FastifyRequest, context: any) => {
+            const time = context.after.toLowerCase();
+            return {
+                statusCode: 429,
+                error: "Too Many Requests",
+                message: `Slow down! Please try again in ${time}.`,
+            };
+        },
     };
 };
+
 export default fastifyRateLimit;
