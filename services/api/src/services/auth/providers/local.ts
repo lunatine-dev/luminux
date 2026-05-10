ea4f991d6b5fdbc93cc67244638ae9e4059629d8
Lunatine <github@lunatine.dev>
2026-05-10 16:22:17 +0100

    refactor(auth): Added logic to stop someone accidentally getting into a passwordless oauth  user

--- services/api/src/services/auth/providers/local.ts
+++ services/api/src/services/auth/providers/local.ts
@@ -4,14 +4,18 @@ import { AppError } from "@core/errors";
 import { issueRefreshToken } from "@services/auth/session-service";
 
 export const processLocalAuth = async (email: string, password: string, ip: string, ua: string) => {
+    if (!password) throw new AppError("Invalid email or password", 401);
+
     const user = await UserModel.findOne({
         email,
     }).select("+password");
 
-    const dbHash = user ? user.password : getDummyHash();
+    const dbHash = user && user.password ? user.password : getDummyHash();
     const isValid = await verify(dbHash, password);
 
-    if (!user || !isValid) throw new AppError("Invalid email or password", 401);
+    if (!user || !user.password || !isValid) {
+        throw new AppError("Invalid email or password", 401);
+    }
 
     const { rawToken, sessionId } = await issueRefreshToken(user._id, ip, ua);
 
