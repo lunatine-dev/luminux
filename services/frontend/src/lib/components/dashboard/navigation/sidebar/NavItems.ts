44e5154b4220b970bcf0755bcf60a025334d6f8e
Lunatine <github@lunatine.dev>
2026-05-17 10:11:45 +0100

    refactor(frontend): adjust routes by removing unplanned routes and adding integration routes

--- services/frontend/src/lib/components/dashboard/navigation/sidebar/NavItems.ts
+++ services/frontend/src/lib/components/dashboard/navigation/sidebar/NavItems.ts
@@ -1,12 +1,6 @@
 import IconHome from "@tabler/icons-svelte/icons/home";
 import IconDashboard from "@tabler/icons-svelte/icons/dashboard";
-import IconDeviceTv from "@tabler/icons-svelte/icons/device-tv";
-import IconGraph from "@tabler/icons-svelte/icons/graph";
-import IconUsers from "@tabler/icons-svelte/icons/users";
-import IconSettings from "@tabler/icons-svelte/icons/settings";
-import IconCloudLock from "@tabler/icons-svelte/icons/cloud-lock";
-import IconVideo from "@tabler/icons-svelte/icons/video";
-import IconPalette from "@tabler/icons-svelte/icons/palette";
+import IconPlug from "@tabler/icons-svelte/icons/plug";
 
 export const items = [
     {
@@ -17,26 +11,13 @@ export const items = [
         ],
     },
     {
-        title: "Live Tools",
-        items: [
-            { title: "Stream Control", url: "/studio/live", icon: IconDeviceTv },
-            { title: "Overlays", url: "/studio/overlays", icon: IconPalette },
-            { title: "Alerts", url: "/studio/alerts", icon: IconCloudLock },
-        ],
-    },
-    {
-        title: "Management",
-        items: [
-            { title: "Clips & VODs", url: "/studio/content", icon: IconVideo },
-            { title: "Community", url: "/studio/community", icon: IconUsers },
-        ],
-    },
-    {
-        title: "Analytics",
-        items: [{ title: "Performance", url: "/studio/stats", icon: IconGraph }],
-    },
-    {
         title: "System",
-        items: [{ title: "Settings", url: "/settings", icon: IconSettings }],
+        items: [
+            {
+                title: "Integrations",
+                url: "/integrations",
+                icon: IconPlug,
+            },
+        ],
     },
 ];
